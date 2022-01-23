6be1c667e82eff887d8d9c949cd13acd1de30a62
Alex M <alexmunoz91@gmail.com>
2022-01-23 10:50:42 +0100

    Add components to app: - Add Header component - Add Home component - Add Footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Home from './views/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <p>This is the app</p>
-    </div>
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
