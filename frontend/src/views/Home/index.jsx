16a681911979eb97d38a43021f39d2551c046be1
Alex M <alexmunoz91@gmail.com>
2022-01-23 16:41:59 +0100

    Add content to Home view component

--- frontend/src/views/Home/index.jsx
+++ frontend/src/views/Home/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.scss';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Home view works</h1>
+    <main className="home-view-component">
+      <h1>Welcome to the beLike intranet</h1>
+      <div>
+        <p>Register or Login To access</p>
+        <Link to="/register">
+          <button type="button" className="btn btn-primary">
+            Register
+          </button>
+        </Link>
+        <Link to="/login">
+          <button type="button" className="btn btn-success">
+            Login
+          </button>
+        </Link>
+      </div>
     </main>
   );
 }
