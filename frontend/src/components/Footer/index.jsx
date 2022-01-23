5fa41eae5eaf2e982e070591c57f3828ed78c0c0
Alex M <alexmunoz91@gmail.com>
2022-01-23 11:45:53 +0100

    Add Footer styles

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './Footer.scss';
+
 export default function Footer() {
   return (
     <footer>
-      <span>Footer Works</span>
+      <span>BeLike User Story&apos;s 2022 © Made with ♡ by Alex Muñoz</span>
     </footer>
   );
 }
