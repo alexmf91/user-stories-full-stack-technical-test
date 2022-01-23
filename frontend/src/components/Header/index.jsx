8b108a96b9c02f3b956346ff3b8edfd97d94be30
Alex M <alexmunoz91@gmail.com>
2022-01-23 16:09:37 +0100

    Add logo and first styles

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import beLikeLogo from '../../assets/belike-logo.PNG';
+
+import './Header.scss';
+
 export default function Header() {
   return (
     <header>
+      <img src={beLikeLogo} alt="belike-logo-img" />
       <span>Header Works</span>
     </header>
   );
