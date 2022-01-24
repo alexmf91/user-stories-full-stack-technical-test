a133c06a5dd19bd92569ce64baec84d8417e17c4
Alex M <alexmunoz91@gmail.com>
2022-01-24 08:56:24 +0100

    Add passport config and userRouter

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,20 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 require('./src/config/database');
+require('./src/config/passport-strategy/localStrategy');
 
 const server = express();
 const port = process.env.PORT || 5000;
 
+require('./src/config/passportConfig')(server);
+
 server.use(cors());
 server.use(morgan('dev'));
 server.use(express.json());
+
+const userRouter = require('./src/routes/user');
+
+server.use('/api', userRouter);
 
 server.listen(port, () =>
   debug(`Server is running on ${chalk.magenta(`http://localhost:${port}`)}`),
