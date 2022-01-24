5a4aefd1ed411b9ae5a4bf2e1df9f08abeaf920d
Alex M <alexmunoz91@gmail.com>
2022-01-24 21:31:55 +0100

    Add username and logout display funcionality

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import beLikeLogo from '../../assets/belike-logo.PNG';
+
+import { authLogout } from '../../redux/actions/actionCreators/auth';
 
 import './Header.scss';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const { user, isLoggedIn } = useSelector((state) => state.auth);
+
+  function handleLogOut() {
+    dispatch(authLogout());
+  }
+
   return (
     <header>
-      <img src={beLikeLogo} alt="belike-logo-img" />
-      <span>Header Works</span>
+      <Link to="/">
+        <img src={beLikeLogo} alt="belike-logo-img" />
+      </Link>
+      {(isLoggedIn && user?.userInfo) && (
+        <div>
+          <span>
+            Hi,
+            {' '}
+            {user.userInfo?.userName}
+          </span>
+          <Link to="/">
+            <button type="button" onClick={handleLogOut} className="btn btn-outline-primary mx-3">
+              LogOut
+            </button>
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
