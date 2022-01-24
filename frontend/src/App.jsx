6ea52f64146b5d19c1d94fd4aa071017bba32d55
Alex M <alexmunoz91@gmail.com>
2022-01-24 21:33:42 +0100

    Add LoginForm and Profile routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './views/Home';
 import RegisterForm from './views/RegisterForm';
+import LoginForm from './views/LoginForm';
+import Profile from './views/Profile';
 import Footer from './components/Footer';
 
 import './App.scss';
@@ -15,6 +17,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
       <Footer />
     </BrowserRouter>
